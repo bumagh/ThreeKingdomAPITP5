29db2f424ccc0d0af5df521f726803cc060ad2f5
akgstudio <bumaya1024@gmail.com>
2025-01-15 17:25:37 +0800

    完善注册时添加bag和character数据

--- application/api/controller/v1/RegAdmin.php
+++ application/api/controller/v1/RegAdmin.php
@@ -4,10 +4,13 @@ namespace app\api\controller\v1;
 
 use app\api\controller\Cross;
 use app\common\model\AdminModel;
+use app\common\model\BagModel;
+use app\common\model\CharacterModel;
 use app\common\model\InviteModel;
 use Firebase\JWT\JWT;
 use think\Controller;
 use think\Request;
+use think\db;
 
 class RegAdmin extends Cross
 {
@@ -20,6 +23,7 @@ class RegAdmin extends Cross
     {
 
         $data = $request->param();
+
         if (isset($data['invitecode']) && empty($data['invitecode'])) {
             return json(['code' => 1, 'msg' => '注册失败,邀请码不能为空']);
         }
@@ -35,11 +39,32 @@ class RegAdmin extends Cross
             return json(['code' => 3, 'msg' => '注册失败,账号已存在']);
         }
         $data['password'] = md5($data['password']);
-        $res = $db->save($data);
-        if ($res) {
-            return json(['code' => 0, 'msg' => '注册成功']);
-        } else
+        // $res = $db->save($data);
+        $res = Db::name('admin')->insertGetId([
+            'username' => $data['username'],
+            'password' => $data['password'],
+        ]);
+        if (!$res)
             return json(['code' => 4, 'msg' => '注册失败,未知错误001']);
+        //先创建背包
+        $bagId =  Db::name('bag')->insertGetId([
+            'status' => 1,
+        ]);
+        if (!$bagId)
+            return json(['code' => 4, 'msg' => '注册失败,未知错误003']);
+        //创建一个角色
+        $dbCharacter = new CharacterModel();
+        $newData = [
+            'admin_id' => $res,
+            'name' => "玩家" . substr($data['username'], 0, 3),
+            'bag_id' => $bagId
+        ];
+        $res = $dbCharacter->_update($newData);
+        if (!$res) {
+            return json(['code' => 4, 'msg' => '注册失败,未知错误002']);
+        }
+
+        return json(['code' => 0, 'msg' => '注册成功']);
         // if ($info['password'] != md5($data['password'])) {
         //     return json(['code' => 0, 'msg' => '账号或密码错误']);
         // }
